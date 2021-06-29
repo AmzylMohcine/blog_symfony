db005aaf535a7eb4e6a64978daffae81c911e911
AmzylMohcine <amzylmohcine@gmail.com>
2021-06-29 15:52:19 +0200

    added comments to article and login logout

---
+++ .history/src/Controller/BlogController_20210628220346.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\User;
+use App\Form\ArticleType;
+use App\Form\CommentType;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\VarDumper\VarDumper;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="app_blog")
+     */
+    //ArticleRepository $repo  -> va nous chercher es donnes dasn la base de données 
+    public function index(ArticleRepository $repo, Request $request, EntityManagerInterface $manager): Response
+    {
+        //$repo = $this->getDoctrine()->getRepository(Article::class); // select de la base de données 
+        $articles = $repo->findAll(); // lister toutt les articles
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles, // les passer au twig 
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+
+        return $this->render('blog/home.html.twig');
+    }
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+
+    public function form(Article $article = null,    Request $request, EntityManagerInterface $manager)
+    {
+        if (!$article) {
+            $article = new Article();
+        }
+
+        /*    $form = $this->createFormBuilder($article)
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->getform(); */
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$article->getId()) {
+                $article->setCreatedAt(new \DateTimeImmutable());
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(), // create vienw cree un petit aspect d'affichage on va le passer a twig
+            'editMode' => $article->getId() !== null,
+
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+
+    public function show(ArticleRepository $repo, $id, Comment $comment = null,  Request $request, EntityManagerInterface $manager)
+    {
+
+        $comment = new Comment();
+
+        $formComment = $this->createForm(CommentType::class, $comment);
+        $formComment->handleRequest($request);
+
+        if ($formComment->isSubmitted() && $formComment->isValid()) {
+            $comment->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($comment);
+            $manager->flush();
+
+
+            $article = $repo->find($id);
+            return $this->render('blog/show.html.twig', [
+                'article' => $article,
+                'formComment' => $formComment->createView()
+            ]);
+        }
+    }
+}
---
+++ .history/src/Controller/SecurityController_20210628174002.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/inscription" , name= "security")
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher)
+    {
+
+        $user = new User();
+
+        $formRegister = $this->createForm(RegisterType::class, $user);
+        $formRegister->handleRequest($request);
+
+        if ($formRegister->isSubmitted() && $formRegister->isValid()) {
+
+            $hash = $hasher->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush($user);
+
+            return $this->redirectToRoute('blog_show');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'formRegister' => $formRegister->createView(),
+        ]);
+    }
+
+       /**
+     * @Route("/inscription" , name= "security")
+     */
+
+     
+
+    public function login()
+    {
+
+        return $this->render('security/login.html.twig');
+    }
+}
